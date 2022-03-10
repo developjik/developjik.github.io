9921e28bee14fd4950b38aa09bc10aaca76595f5
developjik <developjik@gmail.com>
2022-03-11 02:18:57 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1b8c10c4781f5193d35f.js"
+    "url": "webpack-runtime-2558eadd39f86d602a6d.js"
   },
   {
-    "url": "styles.df38b158323be6c050c3.css"
+    "url": "styles.2e60b6b748fdb6175bd1.css"
   },
   {
     "url": "framework-0e1bf37de02f7cef2174.js"
   },
   {
-    "url": "app-b4b95fee99f9e0e6d65d.js"
+    "url": "app-aeb0eee2521c2a210b50.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "250fa5c46b84b22a30063fc2a8db5d4e"
+    "revision": "c2fac249ea9cdd8be538afa5b07ed9c4"
   },
   {
     "url": "static/webfonts/s/roboto/v29/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -59,7 +59,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "4d23ecd33747f0d266d718dbd156c745"
+    "revision": "26cbde78d54042bf98cb777d2b278cbb"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-b4b95fee99f9e0e6d65d.js`))) {
+  if (!resources || !(await caches.match(`/app-aeb0eee2521c2a210b50.js`))) {
     return await fetch(event.request)
   }
 
