c4a79bbd4725b4caa80ca2442046073fc58544e9
developjik <developjik@gmail.com>
2022-03-21 01:02:53 +0900

    feat: utteranceas 댓글 기능 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `developjik/developjik.io`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
